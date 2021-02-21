8ea13b7e0b8ab1fdd2d446277ae3e355b7d19b1b
Bruno Tavares <connect+github@bltavares.com>
2021-02-20 22:17:56 -0300

    Fix the error of typescript loading a white screen on electron

--- native/preload.js
+++ native/preload.js
@@ -7,3 +7,6 @@ window.electron = {
   isDev: process.env.NODE_ENV === "development",
   isExperimental: process.env.RADICLE_UPSTREAM_EXPERIMENTAL === "true",
 };
+
+// https://github.com/electron/electron/issues/2863#issuecomment-479186008
+window.exports = {"__esModule": true};
